237f46aa53f01474a710e260ee059ec493f4f884
Thiruch Rakthao <sax_inw_com@hotmail.com>
2016-12-07 21:32:20 +0700

    Decision Tree: fix bug convert string float

--- AI.py
+++ AI.py
@@ -16,52 +16,17 @@ new_data = data.copy()
 for i in range(0, len(data.columns)):
     le = preprocessing.LabelEncoder()
     le.fit(data.ix[:, i])
-
-    data.ix[:, i] = le.transform(data.ix[:, i])
-    
-data_target = data['class']
-data_feature = data.drop('class', 1)
-data_feature = data_feature.drop('bruises',1)
-data_feature = data_feature.drop('spore-print-color',1)
-data_feature = data_feature.drop('odor',1)
-data_feature = data_feature.drop('stalk-root',1)
-
-
-
-# In[34]:
-
-cfl = DecisionTreeClassifier()
-predict = cross_val_score(cfl, data_feature, data_target, scoring='f1', cv=5)
-print(predict)
-
-
-# In[35]:
-
-# Training classifiers
-clf1 = DecisionTreeClassifier()
-clf2 = KNeighborsClassifier(n_neighbors=7)
-clf3 = SVC(kernel='rbf', probability=True)
-eclf = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2),
-                                    ('svc', clf3)],
-                        voting='soft', weights=[2, 1, 2])
-
-
-# In[36]:
-
-clf1.fit(data_feature, data_target)
-clf2.fit(data_feature, data_target)
-clf3.fit(data_feature, data_target)
-eclf.fit(data_feature, data_target)
-
-
     new_data.ix[:, i] = le.transform(data.ix[:, i])
-
 
 
 # print (data.drop('class', 1))
 
 data_target = new_data['class']
 data_feature = new_data.drop('class', 1)
+data_feature = data_feature.drop('bruises',1)
+data_feature = data_feature.drop('spore-print-color',1)
+data_feature = data_feature.drop('odor',1)
+data_feature = data_feature.drop('stalk-root',1)
 
 cfl = tree.DecisionTreeClassifier()
 
